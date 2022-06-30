a038cee64c8a7c4b3ec0eb70655ec61557fc7fc6
Geoffrey Arner <geoffarner5@gmail.com>
2022-06-30 11:36:44 -0500

    Defined products

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  def 
+  def products {:name "string", :price "integer", :image_url "string", :description "string"}
+  end
 end
+#name, price (integer), image_url, and description
