a9f94ee259cc21015389bc48b32e1b9062bc5923
Geoffrey Arner <geoffarner5@gmail.com>
2022-06-30 11:48:35 -0500

    Product controller revised

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,8 @@
-class ApplicationController < ActionController::API
+class ProductController < ActionController::API
+  def products(options)
+    @name = options(:name)
+    @price = options(:price)
+    @image_url = options(:image_url)
+    @description = options(:description)
+  end
 end
