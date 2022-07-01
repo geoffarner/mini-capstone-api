6ea9af6e4d894b6c48526dbc8584e157e57bb2f5
Geoffrey Arner <geoffarner5@gmail.com>
2022-07-01 14:53:36 -0500

    never works seeds

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,34 @@ class ProductsController < ActionController::API
 
     render json: products.as_json
   end
+
+  def index
+    products = Product.all
+    render json: products.as_json
+  end
+
+  def show
+    product_id = params["id"]
+    product = Product.find_by(id: product_id)
+    render json: products.as_json
+  end
+
+  def create
+    Product.create(name: params["name"], price: params["price"], image_url: params["image_url"], description: params["description"])
+    render json: products.as_json
+  end
+
+  def update
+    product_id = params["id"]
+    product = Product.find_by(id: product_id)
+
+    product.name = params["name"] || product.name
+    product.price = params["price"] || product.price
+    product.description = params["description"] || product.description
+    product.image_url = params["image_url"] || product.image_url
+    product.save
+  end
+
+  def destroy
+    product_id = params["id"]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  get "/products_method", controller: "products", action: "products_method"
+  #get "/products_method", controller: "products", action: "products_method"
+  get "/products" => "products#index"
+
+  get "/products/:id" => "products#show"
+
+  post "/products/" => "products#create"
+
+  patch "/products/:id" => "products#update"
+
+  delete "/products/:id" => "products#destroy"
 end
+
 #name, price (integer), image_url, and description
