3ec6cfaceab95cc5858ceef8077be43573dab641
Geoffrey Arner <geoffarner5@gmail.com>
2022-07-01 08:37:12 -0500

    Make a database phase 2

--- db/seeds.rb
+++
@@ -1,1 +0,0 @@
-Product.create ({ name: "phone", price: 300, image_url: "https://images.unsplash.com/photo-1571239982287-9a7a17c1b9c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2VsbHBob25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60", description: "makes calls" })
